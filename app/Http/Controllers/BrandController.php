9cce0c28bbad6ed9b38dc2905a89203242883e32
YoussefLMT <mcyoussef324@gmail.com>
2022-11-02 12:32:43 +0100

    add AddNewBrand function

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class BrandController extends Controller
 {
-    //
+    
+    public function AddNewBrand(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'=> 'required',
+        ]);
+
+        if($validator->fails()){
+
+            return response()->json([
+                'validation_err' => $validator->messages(),
+            ]);
+
+        }else{
+
+            Brand::create([
+                'name'=> $request->name,
+            ]);
+    
+            return response()->json([
+                'status' => 200,
+                'message' => 'Brand added successfully',
+            ]);
+        }
+    }
 }
