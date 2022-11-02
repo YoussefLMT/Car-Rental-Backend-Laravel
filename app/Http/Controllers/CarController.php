90ae9174bdd4a6531ec86e050c11f94f670f405a
YoussefLMT <mcyoussef324@gmail.com>
2022-11-02 20:00:19 +0100

    add addNewECar function

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -6,5 +6,38 @@ use Illuminate\Http\Request;
 
 class CarController extends Controller
 {
-    //
+    public function addNewECar(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'=> 'required',
+            'model_year'=> 'required',
+            'brand' => 'required',
+            'color' => 'required',
+            'capacity' => 'required',
+            'plat_number' => 'required',
+        ]);
+
+        if($validator->fails()){
+
+            return response()->json([
+                'validation_err' => $validator->messages(),
+            ]);
+
+        }else{
+
+            Employee::create([
+                'name'=> $request->name,
+                'model_year' => $request->model_year,
+                'brand'=> $request->brand,
+                'color' => $request->color,
+                'capacity' => $request->capacity,
+                'plat_number' => $request->plat_number,
+            ]);
+    
+            return response()->json([
+                'status' => 200,
+                'message' => "Car added successfully",
+            ]);
+        }
+    }
 }
