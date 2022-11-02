8e50684448064518ba4c61d1aaa9980c23c16a13
YoussefLMT <mcyoussef324@gmail.com>
2022-11-02 20:06:28 +0100

    add updateCar function

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -75,4 +75,54 @@ class CarController extends Controller
             ]);
         }
     }
+
+
+
+    public function updateCar(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'=> 'required',
+            'model_year'=> 'required',
+            'brand' => 'required',
+            'color' => 'required',
+            'capacity' => 'required',
+            'plat_number' => 'required',
+        ]);
+
+
+        if($validator->fails()){
+
+            return response()->json([
+                'status' => 422,
+                'validation_err' => $validator->messages(),
+            ]);
+
+        }else{
+
+            $car = Car::find($id);
+
+            if($car){
+
+                $car->name = $request->name;
+                $car->model_year = $request->model_year;
+                $car->brand = $request->brand;
+                $car->color = $request->color;
+                $car->capacity = $request->capacity;
+                $car->plat_number = $request->plat_number;
+                $car->save();
+        
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Updated successully',
+                ]);
+
+            }else{
+
+                return response()->json([
+                    'status' => 404,
+                    'message' => 'Car not found!',
+                ]);
+            }
+        }
+    }
 }
