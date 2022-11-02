e858edb7fa424b4714214cd62f1834c6c63892fd
YoussefLMT <mcyoussef324@gmail.com>
2022-11-02 19:53:48 +0100

    add cars table

---
+++ database/migrations/2022_11_02_184801_create_cars_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('model_year');
+            $table->string('brand');
+            $table->string('color');
+            $table->string('capacity');
+            $table->string('plate_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
