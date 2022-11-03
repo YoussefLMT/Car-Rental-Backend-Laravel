042d8b115aaa4380729ab227c3966a389b7b5f7a
YoussefLMT <mcyoussef324@gmail.com>
2022-11-03 17:54:38 +0100

    add reservations table

---
+++ database/migrations/2022_11_03_164759_create_reservations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone');
+            $table->integer('car_id');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('total_amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
