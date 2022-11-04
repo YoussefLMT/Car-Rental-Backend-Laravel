c4e440d3261646b607a4774faeb6816f1db63d81
YoussefLMT <mcyoussef324@gmail.com>
2022-11-04 19:56:38 +0100

    add getTotalCount

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class StatisticsController extends Controller
 {
-    //
+    
+    public function getTotalCount(){
+
+        $carsCount = Car::all()->count();
+        $usersCount = User::all()->count();
+        $reservationsCount = Reservation::all()->count();
+        $income = DB::table('reservations')->sum('reservations.total_amount');
+
+        return response()->json([
+            'status' => 200,
+            'carsCount' => $carsCount,
+            'usersCount' => $usersCount,
+            'reservationsCount' => $reservationsCount,
+            'income' => $income
+        ]);
+    }
 }
